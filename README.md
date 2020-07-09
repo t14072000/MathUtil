f2c70877dd42c7a9c2ddfcce5102c27f1293b071
t14072000 <t14072000@gmail.com>
2020-07-09 08:16:25 +0700

    1st upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Welcome to blabla Repository
+
+This repository contains the sample project written in java using for demonstration of Continuous Process(CI)
+
+It is part of Software Engineering course in Semester 4 FPT University, HCMC Campus
+
+## You will find in this repo:
+* MathUtil class
+* JUnit Test (TDD demonstration)
+* getignore file
+
+## Contact me via 
+[GitHub](http://github.com/t14072000)
+
+##### © 2020 by blabla
---
+++ src/thinh/util/MathUtil.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thinh.util;
+
+/**
+ *
+ * @author Lê Nhựt Thịnh
+ */
+//class nay chua cac ham tien ichde ho tro cac phep tinh toan 
+//ci may la tien ich ham y dung chung cho moi noi 
+//do đó chung may se la static
+public class MathUtil {
+    //hàm giai thừa n!=1.2.3.4....n bùng nổ kết quả rất nhanh
+    //nên tràn int (2tỉ1)sớm, ta trả về long 15!
+    public static long computeFactorial(int n){
+        //cứ viết sai đúng từ từ tính
+        long result=1;
+        for (int i = 1; i <= n; i++) {
+            result*=i;
+        }
+        return result;
+    }
+}

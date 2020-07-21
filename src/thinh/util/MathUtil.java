960f6cf6b447a62e0bf6207dfc68e754bb88ec92
t14072000 <t14072000@gmail.com>
2020-07-21 08:24:24 +0700

    add the exception handler for cF() method

--- src/thinh/util/MathUtil.java
+++ src/thinh/util/MathUtil.java
@@ -17,6 +17,9 @@ public class MathUtil {
     //nên tràn int (2tỉ1)sớm, ta trả về long 15!
     public static long computeFactorial(int n){
         //cứ viết sai đúng từ từ tính
+        if(n<0 || n>15){
+            throw new IllegalArgumentException("Invalid argument. n must >=0 & <=15");
+        }
         long result=1;
         for (int i = 1; i <= n; i++) {
             result*=i;

98702a1122f128f491e191f3d9af754b87a67956
t14072000 <t14072000@gmail.com>
2020-07-21 09:55:01 +0700

    refactor the cF() method by using recursion

--- src/thinh/util/MathUtil.java
+++ src/thinh/util/MathUtil.java
@@ -20,10 +20,15 @@ public class MathUtil {
         if(n<0 || n>15){
             throw new IllegalArgumentException("Invalid argument. n must >=0 & <=15");
         }
-        long result=1;
+        if(n==0||n==1){
+            return 1;
+        }
+        return n*computeFactorial(n-1);
+        /*long result=1;
         for (int i = 1; i <= n; i++) {
             result*=i;
         }
         return result;
+        */
     }
 }
